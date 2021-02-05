0e557fd60e012deaecb94536e43ba371949c6465
Readiz <readiz@readiz.com>
2021-02-05 23:15:02 +0900

    fix 2..

--- src/components/app.js
+++ src/components/app.js
@@ -5,10 +5,8 @@ import Viewer from './viewer';
 
 export default class App extends Component {
   render() {
-    const width = window.innerWidth / 2;
-
     return (
-      <SplitPane split="vertical" defaultSize={width}>
+      <SplitPane split="vertical" defaultSize="50%">
         <Editor />
         <Viewer />
       </SplitPane>
